56c80441ab755499209842df8becea3a0c22cd14
zanynik <36293420-zanynik@users.noreply.replit.com>
2025-01-13 08:25:22 +0000

    Restored to '8decbbb8e6c3a2ca304ad5c06679799b307c4690'
    Replit-Restored-To:8decbbb8e6c3a2ca304ad5c06679799b307c4690

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import App from './App';
 import "./index.css";
@@ -10,10 +9,8 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="light" storageKey="app-theme">
-        <App />
-        <Toaster />
-      </ThemeProvider>
+      <App />
+      <Toaster />
     </QueryClientProvider>
   </StrictMode>,
 );
